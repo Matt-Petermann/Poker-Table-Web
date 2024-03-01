c8abb7789a1b91503c3abfd6273a4ced0e620a78
Matt <matthewp214@gmail.com>
2024-03-01 00:41:54 -0600

    add players around table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Button, ButtonGroup, Card } from "@nextui-org/react";
+import { Avatar, Button, ButtonGroup, Card } from "@nextui-org/react";
 import { FaArrowRightLong, FaArrowRotateLeft, FaTrash, FaUserShield } from "react-icons/fa6";
 
 import table from '../../public/img/table.png';
@@ -13,6 +13,16 @@ export default function Home() {
       </Button>
       <div className="relative h-[100vh]">
         <img src={table.src} className="w-[50vw] pt-[18vh] ml-auto mr-auto" />
+        <Avatar size="lg" className="absolute top-[13vh] left-[23vw]"></Avatar>
+        <Avatar size="lg" className="absolute top-[5vh] left-[38vw]"></Avatar>
+        <Avatar size="lg" className="absolute top-[5vh] right-[38vw]"></Avatar>
+        <Avatar size="lg" className="absolute top-[13vh] right-[23vw]"></Avatar>
+        <Avatar size="lg" className="absolute top-[40vh] right-[17vw]"></Avatar>
+        <Avatar size="lg" className="absolute bottom-[26vh] right-[23vw]"></Avatar>
+        <Avatar size="lg" className="absolute bottom-[20vh] right-[38vw]"></Avatar>
+        <Avatar size="lg" className="absolute bottom-[20vh] left-[38vw]"></Avatar>
+        <Avatar size="lg" className="absolute bottom-[26vh] left-[23vw]"></Avatar>
+        <Avatar size="lg" className="absolute top-[40vh] left-[17vw]"></Avatar>
       </div>
       <Card isBlurred className="fixed border-none left-12 right-12 bottom-4">
         <div>

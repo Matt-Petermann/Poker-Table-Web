8d3cf8b85fcffe0374d2a570e30f23af7b15aa9e
Matt <matthewp214@gmail.com>
2024-11-24 01:25:20 -0600

    back button on admin portal

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,18 @@
+import { Button } from "@nextui-org/react";
+import { FaArrowLeft } from "react-icons/fa6";
+
 export default () => {
-    return <main></main>;
+    return (
+        <main>        
+            <Button
+                    as="a"
+                    color="success"
+                    className="fixed top-2 right-2 z-10"
+                    startContent={<FaArrowLeft />}
+                    href="/"
+                >
+                    Return to Table
+                </Button>
+        </main>
+    );
 }

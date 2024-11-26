dabd77f9b303e3a326263516d8c8bcb92e748c6e
Matt <matthewp214@gmail.com>
2024-11-25 20:10:20 -0600

    open modal on scan all button click

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,38 +1,52 @@
 "use client";
 
-import { Button } from "@nextui-org/react";
+import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/react";
 import { FaArrowLeft, FaBarcode } from "react-icons/fa6";
 import CardImages from "@/lib/cards";
 
 export default () => {
+    const disclosure = useDisclosure();
+
     return (
-        <main className="pt-4 space-y-8">
-            <Button
-                as="a"
-                color="success"
-                className="fixed top-2 right-2 z-10"
-                startContent={<FaArrowLeft />}
-                href="/"
-            >
-                Return to Table
-            </Button>
-            <div className="flex">
+        <>
+            <Modal {...disclosure} hideCloseButton size="4xl" isDismissable={false}>
+                <ModalContent className="backdrop-blur-md bg-opacity-25">
+                    <ModalHeader>Scan all Cards</ModalHeader>
+                    <ModalBody>Stepper, Card Image</ModalBody>
+                    <ModalFooter>Cancel, Save</ModalFooter>
+                </ModalContent>
+            </Modal>
+            <main className="pt-4 space-y-8">
                 <Button
-                    size="lg"
-                    color="primary"
-                    startContent={<FaBarcode />}
-                    className="mx-auto"
-                >Scan All</Button>
-            </div>
-            <div className="grid grid-cols-6 w-3/4 mx-auto">
-                {CardImages.map((img, idx) => (
-                    <img
-                        src={img.src}
-                        className="inline-block p-2"
-                        key={idx}
-                    />
-                ))}
-            </div>
-        </main>
+                    as="a"
+                    color="success"
+                    className="fixed top-2 right-2 z-10"
+                    startContent={<FaArrowLeft />}
+                    href="/"
+                >
+                    Return to Table
+                </Button>
+                <div className="flex">
+                    <Button
+                        size="lg"
+                        color="primary"
+                        startContent={<FaBarcode />}
+                        className="mx-auto"
+                        onClick={() => disclosure.onOpen()}
+                    >
+                        Scan All
+                    </Button>
+                </div>
+                <div className="grid grid-cols-6 w-3/4 mx-auto">
+                    {CardImages.map((img, idx) => (
+                        <img
+                            src={img.src}
+                            className="inline-block p-2"
+                            key={idx}
+                        />
+                    ))}
+                </div>
+            </main>
+        </>
     );
 }
